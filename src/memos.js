5210d3e36387f371dcd542699c374b721304972c
beffiy.com <mail@beffiy.com>
2024-08-22 16:49:49 +0800

    initial commit

---
+++ src/memos.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const axios = require("axios").default;
+
+const site = popclip.options.site;
+const access_token = popclip.options.access_token;
+const url = `${site}/api/v1/memos`;
+
+
+const headers = {
+  Accept: "application/json",
+  Authorization: `Bearer ${access_token}`,
+};
+
+let content = "\n" + popclip.input.markdown.trim();
+  if (popclip.options.sourceLink && popclip.context && popclip.context.browserUrl) {
+    content += `\n[${popclip.context.browserTitle || "Source"}](${popclip.context.browserUrl})`;
+  }
+
+const data = {
+  content: content,
+  visibility: 'Private'
+};
+
+axios
+  .post(url, data, { headers })
+  .then((res) => {
+    // print("Memo created successfully", res.data);
+    popclip.showSuccess();
+  })
+  .catch((err) => {
+    popclip.showFailure();
+    console.error("Error creating memo", err);
+  });
